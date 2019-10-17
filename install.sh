284fbc0e8fbacb75adc0f3ecb5d500c415f3e602
Guillermo Kuster <guillermo@techsniffer.me>
2019-10-16 23:47:37 -0300

    Add symlink of the folder in $HOME so the plugins_config can be loaded

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ source $DOTFILES_ROOT/vimrcs/filetypes.vim
 source $DOTFILES_ROOT/vimrcs/plugins_config.vim
 source $DOTFILES_ROOT/vimrcs/extended.vim" > ~/.vimrc
 
+# Create symlink for plugins_config to load
+ln -s $DOTFILES_ROOT $HOME/.vim-environment
+
 echo "Full configuration set. Happy coding :-)"
